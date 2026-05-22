ecefe5e786b53d9b9603f93be427070e595075db
shimatoshi <196684849+shimatoshi@users.noreply.github.com>
2026-05-23 08:30:07 +0900

    fix: SW activate時に他PWAのキャッシュを巻き添え削除するバグ修正

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+    caches.keys().then(keys => Promise.all(keys.filter(k => k.startsWith('news-') && k !== CACHE_NAME).map(k => caches.delete(k))))
   );
   self.clients.claim();
 });
